95bc2f8451c11e7ed38ef0cca3add6ed7b38d855
ShwRat410 <shwetaratnawat410@gmail.com>
2025-02-16 17:34:59 +0000

    Added Result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header"
 import UserInput from "./components/UserInput"
+import Result from "./components/Result";
 
 const INITIAL_VALUES={
   initialInvestment:10000,
@@ -26,6 +27,7 @@ function App() {
     <>
     <Header></Header>
     <UserInput onUserInput={handleInputValue} inputValue={inputValue}></UserInput>
+    <Result userInput={inputValue}></Result>
     </>
   )
 }
---
+++ src/components/Result.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Result({userInput}) {
+  return (
+    <div>
+      <p>Result {userInput.annualInvestment}</p>
+    </div>
+  )
+}
