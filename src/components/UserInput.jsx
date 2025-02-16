11f58879c3454c8e3976152266910e11d9ac7e91
ShwRat410 <shwetaratnawat410@gmail.com>
2025-02-16 17:15:43 +0000

    Added two-way binding

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const INITIAL_VALUES={
+    initialInvestment:1000,
+    annualInvestment:23,
+    expectedReturn:10,
+    duration:2
+}
 
 export default function UserInput() {
+
+  const[inputValue,setInputValue]=useState(INITIAL_VALUES);  
+
+  function handleInputValue(identifier,newValue){
+    setInputValue((prevInputValue)=>{
+        return({
+            ...prevInputValue,
+            [identifier]:newValue
+        })
+    })
+  }
+
   return (
     <section id="user-input">
         <div className="input-group">
             <p>
                 <label>Initial investment</label>
-                <input type='number'/>
+                <input type='number' required onChange={(event)=>{handleInputValue('initialInvestment',event.target.value)}} value={inputValue.initialInvestment}/>
             </p>
             <p>
                 <label>Annual investment</label>
-                <input type='number'/>
+                <input type='number'required onChange={(event)=>{handleInputValue('annualInvestment',event.target.value)}} value={inputValue.annualInvestment}/>
             </p>
         </div>
         <div className="input-group">
             <p>
                 <label>Expected Return</label>
-                <input type='number'/>
+                <input type='number'required onChange={(event)=>{handleInputValue('expectedReturn',event.target.value)}} value={inputValue.expectedReturn}/>
             </p>
             <p>
                 <label>Duration</label>
-                <input type='number'/>
+                <input type='number'required onChange={(event)=>{handleInputValue('duration',event.target.value)}} value={inputValue.duration}/>
             </p>
         </div>
     </section>
