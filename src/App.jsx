efc36c0c661e1bc170a48f8169bb6a8c8e220af1
ShwRat410 <shwetaratnawat410@gmail.com>
2025-02-16 18:18:53 +0000

    Added +/parseInt for properly handling number values

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     setInputValue((prevInputValue)=>{
         return({
             ...prevInputValue,
-            [identifier]:newValue
+            [identifier]:+newValue
         })
     })
   }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {calculateInvestmentResults} from '../util/investment'
 
 export default function Result({userInput}) {
+  const calculatedResult = calculateInvestmentResults(userInput);
   return (
     <div>
+      {console.log(calculatedResult)}
+      
       <p>Result {userInput.annualInvestment}</p>
     </div>
   )
