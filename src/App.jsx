9d4d2f1ddcf7efc8cc00bf4dbeadb2a029b503ee
ShwRat410 <shwetaratnawat410@gmail.com>
2025-02-16 17:31:40 +0000

    Lifted state up from UserInput to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import Header from "./components/Header"
 import UserInput from "./components/UserInput"
 
+const INITIAL_VALUES={
+  initialInvestment:10000,
+  annualInvestment:1200,
+  expectedReturn:6,
+  duration:10
+}
+
 function App() {
+  
+  const[inputValue,setInputValue]=useState(INITIAL_VALUES);  
+
+  function handleInputValue(identifier,newValue){
+    setInputValue((prevInputValue)=>{
+        return({
+            ...prevInputValue,
+            [identifier]:newValue
+        })
+    })
+  }
+
   return (
     <>
     <Header></Header>
-    <UserInput></UserInput>
+    <UserInput onUserInput={handleInputValue} inputValue={inputValue}></UserInput>
     </>
   )
 }
