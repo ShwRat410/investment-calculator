d333de378d39daef8918e2dc9355ced033025c9c
ShwRat410 <shwetaratnawat410@gmail.com>
2025-02-16 16:49:18 +0000

    Added UserInput Component

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function UserInput() {
+  return (
+    <section id="user-input">
+        <div className="input-group">
+            <p>
+                <label>Initial investment</label>
+                <input type='number'/>
+            </p>
+            <p>
+                <label>Annual investment</label>
+                <input type='number'/>
+            </p>
+        </div>
+        <div className="input-group">
+            <p>
+                <label>Expected Return</label>
+                <input type='number'/>
+            </p>
+            <p>
+                <label>Duration</label>
+                <input type='number'/>
+            </p>
+        </div>
+    </section>
+  )
+}
